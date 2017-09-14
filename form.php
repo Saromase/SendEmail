a339ea29247bb36f7bcf8111ce47c6314604c508
acima <acimakd@gmail.com>
2017-09-14 09:54:33 +0200

    test

--- form.php
+++ form.php
@@ -1,6 +1,5 @@
 <?php 
 include 'function.php';
-
 $name = $_POST['name'];
 $firstname = $_POST['first_name'];
 $mail = $_POST['mail'];
@@ -9,5 +8,7 @@ $animal = $_POST['animal'];
 $nameAnimal = $_POST['name_animal'];
 $message = $_POST['message'];
 insertIntoBDD($name, $firstname, $mail, $date, $animal, $nameAnimal, $message);
+$userID = selectUserID($name, $date, $message);
+/*sendMail($name, $mail, $date, $userID['id']);*/
 header('Location: index.php');
 ?>
